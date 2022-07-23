7d8e21c7f44be04d9db8d117190e670bdcec41b4
radostBeZuma <romahasasha@yandex.ru>
2022-07-23 16:31:26 +0300

    add slide accordion

--- blocks/our-team/our-team.js
+++ blocks/our-team/our-team.js
@@ -3,7 +3,11 @@ import Swiper from 'swiper/bundle';
 
 document.addEventListener("DOMContentLoaded", () => {
 
-	const sliderMin = new Swiper('.our-team__slider-min', {
+	const block = 'our-team';
+
+	// slider
+
+	const sliderMin = new Swiper(`.${block}__slider-min`, {
 		speed: 1000,
 		effect: 'fade',
 		fadeEffect: {
@@ -11,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		},
 	});
 
-	const sliderText = new Swiper('.our-team__text-slider', {
+	const sliderText = new Swiper(`.${block}__text-slider`, {
 		speed: 1000,
 		effect: 'fade',
 		fadeEffect: {
@@ -19,12 +23,11 @@ document.addEventListener("DOMContentLoaded", () => {
 		},
 	});
 
-
-	const sliderLarge = new Swiper('.our-team__box-slider-large', {
+	const sliderLarge = new Swiper(`.${block}__box-slider-large`, {
 		speed: 1000,
 		navigation: {
-			prevEl: '.our-team__box-btn--prev',
-			nextEl: '.our-team__box-btn--next',
+			prevEl: `.${block}__box-btn--prev`,
+			nextEl: `.${block}__box-btn--next`,
 			disabledClass: '_btn-hide',
 		},
 		effect: 'fade',
@@ -50,5 +53,53 @@ document.addEventListener("DOMContentLoaded", () => {
 			sliderLarge.slideNext();
 		}
 	});
+
+	// slider end
+
+	// cards
+
+	/* (по логике здесь необходимо сделать так чтобы высота задавалась при Resize,
+	а также throttle сделать для того, чтобы не вызывался Resize на каждом пикселе) */
+
+	/*Если от той высоты, которую занимает блок (самый большой) при том, когда он hidden, 
+	сделать быстрый просмотр его высоты, когда он открыт и откусить это расстояние от него, 
+	то выстоу можно будет задать ту, которую он займет (пока это лучшее что смог придумать)*/
+
+	const arrowBtn = document.querySelectorAll(`.${block}__el-card-btn`);
+
+	const cards = document.querySelectorAll(`.${block}__box-card`);
+
+	let lastHeight = 0;
+
+	const box = document.querySelector(`.${block}__box-cards`);
+
+	cards.forEach(element => {
+
+		if (element.scrollHeight > lastHeight) {
+
+			lastHeight = element.scrollHeight;
+		}
+
+	});
+
+	box.style.height = lastHeight + 'px';
+
+	arrowBtn.forEach(element => {
+		element.addEventListener('click', () => {
+
+			const activeCard = document.querySelector(`.${block}__box-card._active`);
+			const parent = element.closest(`.${block}__box-card`);
+
+			if (parent != activeCard) {
+				parent.classList.add('_active');
+				activeCard.classList.remove('_active');
+			}
+		});
+	});
+
+	// cards end
+
+
+
 });
 
--- blocks/our-team/our-team.php
+++ blocks/our-team/our-team.php
@@ -36,13 +36,13 @@
 					<div class="our-team__box-slider-large">
 						<div class="our-team__el-slider-l-wrapper swiper-wrapper">
 							<div class="our-team__el-slider-l-slide swiper-slide">
-								<img class="our-team__person-l-img" src="./../../img/max-d-large.jpg" alt="">
+								<img class="our-team__person-l-img" src="./../../img/max-d-large.jpg" alt="Max D. Chief Executive Officer">
 							</div>
 							<div class="our-team__el-slider-l-slide swiper-slide">
-								<img class="our-team__person-l-img" src="./../../img/alex-k-large.jpg" alt="">
+								<img class="our-team__person-l-img" src="./../../img/alex-k-large.jpg" alt="Alex K. Lorem ipsum pos.">
 							</div>
 							<div class="our-team__el-slider-l-slide swiper-slide">
-								<img class="our-team__person-l-img" src="./../../img/sasha-b-large.jpg" alt="">
+								<img class="our-team__person-l-img" src="./../../img/sasha-b-large.jpg" alt="Sasha B. Lorem pos.">
 							</div>
 						</div>
 					</div>
@@ -57,15 +57,15 @@
 						<div class="our-team__el-slider-min-wrapper swiper-wrapper">
 							<div class="our-team__el-slider-min-slide swiper-slide">
 								<p class="our-team__person-desc-min">Alex K.</p>
-								<img class="our-team__person-min-img" src="./../../img/alex-k-min.jpg" alt="">
+								<img class="our-team__person-min-img" src="./../../img/alex-k-min.jpg" alt="Alex K. Lorem ipsum pos.">
 							</div>
 							<div class="our-team__el-slider-min-slide swiper-slide">
 								<p class="our-team__person-desc-min">Sasha B.</p>
-								<img class="our-team__person-min-img" src="./../../img/sasha-b-min.jpg" alt="">
+								<img class="our-team__person-min-img" src="./../../img/sasha-b-min.jpg" alt="Sasha B. Lorem pos.">
 							</div>
 							<div class="our-team__el-slider-min-slide swiper-slide">
 								<p class="our-team__person-desc-min">Max D.</p>
-								<img class="our-team__person-min-img" src="./../../img/max-d-min.jpg" alt="">
+								<img class="our-team__person-min-img" src="./../../img/max-d-min.jpg" alt="Max D. Chief Executive Officer">
 							</div>
 						</div>
 					</div>
@@ -193,6 +193,170 @@
 						</div>
 						
 					</div>
+				</div>
+			</div>
+
+			<div class="our-team__box-cards">
+				<div class="our-team__box-card _active">
+					<div class="our-team__el-card-inner">
+						<div class="our-team__el-img-n-arrow">
+							<div class="our-team__box-card-person-img">
+								<img class="our-team__el-card-img" src="./../../img/max-d-large.jpg" alt="">
+							</div>
+							<button class="our-team__el-card-btn" type="button">
+								<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path d="M11.06 5.7265L8 8.77984L4.94 5.7265L4 6.6665L8 10.6665L12 6.6665L11.06 5.7265Z" fill="#111111"/>
+								</svg>
+							</button>
+						</div>
+						<div class="our-team__el-person-name">Max D.</div>
+					</div>
+					<div class="our-team__el-card-all-text">
+						<div class="our-team__el-card-desc">
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic suscipit ducimus voluptate aliquam eos ab nostrum assumenda eveniet voluptates odio laboriosam, quo eum vitae reiciendis tempore sequi perferendis at debitis.</p>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil ea corrupti tempore reprehenderit! Culpa et sit rerum dolores dolore officiis, aut ullam corrupti reprehenderit. Eligendi doloribus accusantium explicabo quaerat voluptatem?</p>
+						</div>
+						<p class="our-team__el-card-person-position">Lorem, ipsum.</p>
+						<div class="our-team__el-card-person-social">
+							<div class="our-team__box-social">
+								<a class="our-team__social-link" href="#">
+									<div class="our-team__social-wrap">
+										<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M15 3.84826C14.4847 4.06689 13.9318 4.21516 13.3509 4.28135C13.9441 3.94125 14.3981 3.40179 14.6133 2.76097C14.0569 3.07593 13.4428 3.30463 12.7884 3.4286C12.2644 2.89334 11.519 2.55994 10.6923 2.55994C9.1062 2.55994 7.82018 3.79131 7.82018 5.30917C7.82018 5.52444 7.84555 5.7347 7.89455 5.93576C5.50801 5.82098 3.39178 4.72615 1.97543 3.06252C1.72784 3.46797 1.58699 3.94039 1.58699 4.44469C1.58699 5.3988 2.0944 6.24066 2.86427 6.73319C2.3936 6.71812 1.95093 6.59415 1.56339 6.38807V6.42242C1.56339 7.75431 2.5537 8.8659 3.86682 9.11889C3.62624 9.18088 3.37255 9.21522 3.11009 9.21522C2.92464 9.21522 2.74529 9.19763 2.56944 9.16412C2.93511 10.2573 3.99543 11.0522 5.25171 11.074C4.26926 11.8112 3.03048 12.2493 1.68499 12.2493C1.45317 12.2493 1.22482 12.2359 1 12.2116C2.27114 12.9931 3.78024 13.4488 5.40218 13.4488C10.6853 13.4488 13.5732 9.25879 13.5732 5.62498L13.5635 5.26898C14.1278 4.88362 14.6159 4.39946 15 3.84826Z" fill="#111111"/>
+										</svg>
+									</div>
+								</a>
+							</div>
+							<div class="our-team__box-social">
+								<a class="our-team__social-link" href="#">
+									<div class="our-team__social-wrap">
+										<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path fill-rule="evenodd" clip-rule="evenodd" d="M0.333374 7.00004C0.333374 4.50773 0.333374 3.26158 0.869273 2.33337C1.22035 1.7253 1.7253 1.22035 2.33337 0.869273C3.26158 0.333374 4.50773 0.333374 7.00004 0.333374C9.49235 0.333374 10.7385 0.333374 11.6667 0.869273C12.2748 1.22035 12.7797 1.7253 13.1308 2.33337C13.6667 3.26158 13.6667 4.50773 13.6667 7.00004C13.6667 9.49235 13.6667 10.7385 13.1308 11.6667C12.7797 12.2748 12.2748 12.7797 11.6667 13.1308C10.7385 13.6667 9.49235 13.6667 7.00004 13.6667C4.50773 13.6667 3.26158 13.6667 2.33337 13.1308C1.7253 12.7797 1.22035 12.2748 0.869273 11.6667C0.333374 10.7385 0.333374 9.49235 0.333374 7.00004ZM7.00001 3.4706C5.05089 3.4706 3.4706 5.05089 3.4706 7.00001C3.4706 8.94913 5.05089 10.5294 7.00001 10.5294C8.94913 10.5294 10.5294 8.94913 10.5294 7.00001C10.5294 5.05089 8.94913 3.4706 7.00001 3.4706ZM7.00001 9.20589C5.78413 9.20589 4.79413 8.21589 4.79413 7.00001C4.79413 5.78325 5.78413 4.79413 7.00001 4.79413C8.21589 4.79413 9.20589 5.78325 9.20589 7.00001C9.20589 8.21589 8.21589 9.20589 7.00001 9.20589ZM11.2644 3.20591C11.2644 3.46564 11.0539 3.6762 10.7942 3.6762C10.5344 3.6762 10.3239 3.46564 10.3239 3.20591C10.3239 2.94617 10.5344 2.73561 10.7942 2.73561C11.0539 2.73561 11.2644 2.94617 11.2644 3.20591Z" fill="#111111"/>
+										</svg>
+									</div>
+								</a>
+							</div>
+							<div class="our-team__box-social">
+								<a class="our-team__social-link" href="#">
+									<div class="our-team__social-wrap">
+										<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M14.6633 14.6667V14.6661H14.6667V9.77614C14.6667 7.38392 14.1517 5.54114 11.355 5.54114C10.0106 5.54114 9.10833 6.27892 8.74 6.97836H8.70111V5.76447H6.04944V14.6661H8.81055V10.2584C8.81055 9.09781 9.03055 7.97558 10.4678 7.97558C11.8839 7.97558 11.905 9.30003 11.905 10.3328V14.6667H14.6633Z" fill="#111111"/>
+											<path d="M1.55334 5.76501H4.31779V14.6667H1.55334V5.76501Z" fill="#111111"/>
+											<path d="M2.93449 1.33337C2.0506 1.33337 1.33337 2.0506 1.33337 2.93449C1.33337 3.81837 2.0506 4.5506 2.93449 4.5506C3.81837 4.5506 4.5356 3.81837 4.5356 2.93449C4.53504 2.0506 3.81782 1.33337 2.93449 1.33337V1.33337Z" fill="#111111"/>
+										</svg>
+									</div>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="our-team__box-card">
+					<div class="our-team__el-card-inner">
+						<div class="our-team__el-img-n-arrow">
+							<div class="our-team__box-card-person-img">
+								<img class="our-team__el-card-img" src="./../../img/alex-k-large.jpg" alt="">
+							</div>
+							<button class="our-team__el-card-btn" type="button">
+								<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path d="M11.06 5.7265L8 8.77984L4.94 5.7265L4 6.6665L8 10.6665L12 6.6665L11.06 5.7265Z" fill="#111111"/>
+								</svg>
+							</button>
+						</div>
+						<div class="our-team__el-person-name">Alex K.</div>
+					</div>
+					<div class="our-team__el-card-all-text">
+						<div class="our-team__el-card-desc">
+							<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis velit eius repudiandae quo dignissimos non esse, cupiditate accusantium reiciendis necessitatibus nemo odit voluptatum repellat corrupti aperiam ullam at consequatur ut!</p>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo quod dolore pariatur eum facere, veniam non fugiat, officia quidem minima alias in. Placeat, quas repellendus ad laborum suscipit deleniti perspiciatis?</p>
+						</div>
+						<p class="our-team__el-card-person-position">Designer</p>
+						<div class="our-team__el-card-person-social">
+							<div class="our-team__box-social">
+								<a class="our-team__social-link" href="#">
+									<div class="our-team__social-wrap">
+										<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M15 3.84826C14.4847 4.06689 13.9318 4.21516 13.3509 4.28135C13.9441 3.94125 14.3981 3.40179 14.6133 2.76097C14.0569 3.07593 13.4428 3.30463 12.7884 3.4286C12.2644 2.89334 11.519 2.55994 10.6923 2.55994C9.1062 2.55994 7.82018 3.79131 7.82018 5.30917C7.82018 5.52444 7.84555 5.7347 7.89455 5.93576C5.50801 5.82098 3.39178 4.72615 1.97543 3.06252C1.72784 3.46797 1.58699 3.94039 1.58699 4.44469C1.58699 5.3988 2.0944 6.24066 2.86427 6.73319C2.3936 6.71812 1.95093 6.59415 1.56339 6.38807V6.42242C1.56339 7.75431 2.5537 8.8659 3.86682 9.11889C3.62624 9.18088 3.37255 9.21522 3.11009 9.21522C2.92464 9.21522 2.74529 9.19763 2.56944 9.16412C2.93511 10.2573 3.99543 11.0522 5.25171 11.074C4.26926 11.8112 3.03048 12.2493 1.68499 12.2493C1.45317 12.2493 1.22482 12.2359 1 12.2116C2.27114 12.9931 3.78024 13.4488 5.40218 13.4488C10.6853 13.4488 13.5732 9.25879 13.5732 5.62498L13.5635 5.26898C14.1278 4.88362 14.6159 4.39946 15 3.84826Z" fill="#111111"/>
+										</svg>
+									</div>
+								</a>
+							</div>
+							<div class="our-team__box-social">
+								<a class="our-team__social-link" href="#">
+									<div class="our-team__social-wrap">
+										<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path fill-rule="evenodd" clip-rule="evenodd" d="M0.333374 7.00004C0.333374 4.50773 0.333374 3.26158 0.869273 2.33337C1.22035 1.7253 1.7253 1.22035 2.33337 0.869273C3.26158 0.333374 4.50773 0.333374 7.00004 0.333374C9.49235 0.333374 10.7385 0.333374 11.6667 0.869273C12.2748 1.22035 12.7797 1.7253 13.1308 2.33337C13.6667 3.26158 13.6667 4.50773 13.6667 7.00004C13.6667 9.49235 13.6667 10.7385 13.1308 11.6667C12.7797 12.2748 12.2748 12.7797 11.6667 13.1308C10.7385 13.6667 9.49235 13.6667 7.00004 13.6667C4.50773 13.6667 3.26158 13.6667 2.33337 13.1308C1.7253 12.7797 1.22035 12.2748 0.869273 11.6667C0.333374 10.7385 0.333374 9.49235 0.333374 7.00004ZM7.00001 3.4706C5.05089 3.4706 3.4706 5.05089 3.4706 7.00001C3.4706 8.94913 5.05089 10.5294 7.00001 10.5294C8.94913 10.5294 10.5294 8.94913 10.5294 7.00001C10.5294 5.05089 8.94913 3.4706 7.00001 3.4706ZM7.00001 9.20589C5.78413 9.20589 4.79413 8.21589 4.79413 7.00001C4.79413 5.78325 5.78413 4.79413 7.00001 4.79413C8.21589 4.79413 9.20589 5.78325 9.20589 7.00001C9.20589 8.21589 8.21589 9.20589 7.00001 9.20589ZM11.2644 3.20591C11.2644 3.46564 11.0539 3.6762 10.7942 3.6762C10.5344 3.6762 10.3239 3.46564 10.3239 3.20591C10.3239 2.94617 10.5344 2.73561 10.7942 2.73561C11.0539 2.73561 11.2644 2.94617 11.2644 3.20591Z" fill="#111111"/>
+										</svg>
+									</div>
+								</a>
+							</div>
+							<div class="our-team__box-social">
+								<a class="our-team__social-link" href="#">
+									<div class="our-team__social-wrap">
+										<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M14.6633 14.6667V14.6661H14.6667V9.77614C14.6667 7.38392 14.1517 5.54114 11.355 5.54114C10.0106 5.54114 9.10833 6.27892 8.74 6.97836H8.70111V5.76447H6.04944V14.6661H8.81055V10.2584C8.81055 9.09781 9.03055 7.97558 10.4678 7.97558C11.8839 7.97558 11.905 9.30003 11.905 10.3328V14.6667H14.6633Z" fill="#111111"/>
+											<path d="M1.55334 5.76501H4.31779V14.6667H1.55334V5.76501Z" fill="#111111"/>
+											<path d="M2.93449 1.33337C2.0506 1.33337 1.33337 2.0506 1.33337 2.93449C1.33337 3.81837 2.0506 4.5506 2.93449 4.5506C3.81837 4.5506 4.5356 3.81837 4.5356 2.93449C4.53504 2.0506 3.81782 1.33337 2.93449 1.33337V1.33337Z" fill="#111111"/>
+										</svg>
+									</div>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="our-team__box-card">
+					<div class="our-team__el-card-inner">
+						<div class="our-team__el-img-n-arrow">
+							<div class="our-team__box-card-person-img">
+								<img class="our-team__el-card-img" src="./../../img/sasha-b-large.jpg" alt="">
+							</div>
+							<button class="our-team__el-card-btn" type="button">
+								<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path d="M11.06 5.7265L8 8.77984L4.94 5.7265L4 6.6665L8 10.6665L12 6.6665L11.06 5.7265Z" fill="#111111"/>
+								</svg>
+							</button>
+						</div>
+						<div class="our-team__el-person-name">Sasha B.</div>
+					</div>
+					<div class="our-team__el-card-all-text">
+						<div class="our-team__el-card-desc">
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente vitae earum, ratione voluptas maiores, ad et dignissimos non eaque voluptates rem nostrum consequatur minus id quas repellat. Quaerat, aut excepturi.</p>
+							<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores, laborum eveniet deserunt rem autem, omnis, sunt ex molestiae facilis voluptas accusantium quia. Provident doloremque, atque iste rerum magnam laboriosam labore!</p>
+							<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores, laborum eveniet deserunt rem autem, omnis, sunt ex molestiae facilis voluptas accusantium quia. Provident doloremque, atque iste rerum magnam laboriosam labore!</p>
+						</div>
+						<p class="our-team__el-card-person-position">Lorem, ipsum.</p>
+						<div class="our-team__el-card-person-social">
+							<div class="our-team__box-social">
+								<a class="our-team__social-link" href="#">
+									<div class="our-team__social-wrap">
+										<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M15 3.84826C14.4847 4.06689 13.9318 4.21516 13.3509 4.28135C13.9441 3.94125 14.3981 3.40179 14.6133 2.76097C14.0569 3.07593 13.4428 3.30463 12.7884 3.4286C12.2644 2.89334 11.519 2.55994 10.6923 2.55994C9.1062 2.55994 7.82018 3.79131 7.82018 5.30917C7.82018 5.52444 7.84555 5.7347 7.89455 5.93576C5.50801 5.82098 3.39178 4.72615 1.97543 3.06252C1.72784 3.46797 1.58699 3.94039 1.58699 4.44469C1.58699 5.3988 2.0944 6.24066 2.86427 6.73319C2.3936 6.71812 1.95093 6.59415 1.56339 6.38807V6.42242C1.56339 7.75431 2.5537 8.8659 3.86682 9.11889C3.62624 9.18088 3.37255 9.21522 3.11009 9.21522C2.92464 9.21522 2.74529 9.19763 2.56944 9.16412C2.93511 10.2573 3.99543 11.0522 5.25171 11.074C4.26926 11.8112 3.03048 12.2493 1.68499 12.2493C1.45317 12.2493 1.22482 12.2359 1 12.2116C2.27114 12.9931 3.78024 13.4488 5.40218 13.4488C10.6853 13.4488 13.5732 9.25879 13.5732 5.62498L13.5635 5.26898C14.1278 4.88362 14.6159 4.39946 15 3.84826Z" fill="#111111"/>
+										</svg>
+									</div>
+								</a>
+							</div>
+							<div class="our-team__box-social">
+								<a class="our-team__social-link" href="#">
+									<div class="our-team__social-wrap">
+										<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path fill-rule="evenodd" clip-rule="evenodd" d="M0.333374 7.00004C0.333374 4.50773 0.333374 3.26158 0.869273 2.33337C1.22035 1.7253 1.7253 1.22035 2.33337 0.869273C3.26158 0.333374 4.50773 0.333374 7.00004 0.333374C9.49235 0.333374 10.7385 0.333374 11.6667 0.869273C12.2748 1.22035 12.7797 1.7253 13.1308 2.33337C13.6667 3.26158 13.6667 4.50773 13.6667 7.00004C13.6667 9.49235 13.6667 10.7385 13.1308 11.6667C12.7797 12.2748 12.2748 12.7797 11.6667 13.1308C10.7385 13.6667 9.49235 13.6667 7.00004 13.6667C4.50773 13.6667 3.26158 13.6667 2.33337 13.1308C1.7253 12.7797 1.22035 12.2748 0.869273 11.6667C0.333374 10.7385 0.333374 9.49235 0.333374 7.00004ZM7.00001 3.4706C5.05089 3.4706 3.4706 5.05089 3.4706 7.00001C3.4706 8.94913 5.05089 10.5294 7.00001 10.5294C8.94913 10.5294 10.5294 8.94913 10.5294 7.00001C10.5294 5.05089 8.94913 3.4706 7.00001 3.4706ZM7.00001 9.20589C5.78413 9.20589 4.79413 8.21589 4.79413 7.00001C4.79413 5.78325 5.78413 4.79413 7.00001 4.79413C8.21589 4.79413 9.20589 5.78325 9.20589 7.00001C9.20589 8.21589 8.21589 9.20589 7.00001 9.20589ZM11.2644 3.20591C11.2644 3.46564 11.0539 3.6762 10.7942 3.6762C10.5344 3.6762 10.3239 3.46564 10.3239 3.20591C10.3239 2.94617 10.5344 2.73561 10.7942 2.73561C11.0539 2.73561 11.2644 2.94617 11.2644 3.20591Z" fill="#111111"/>
+										</svg>
+									</div>
+								</a>
+							</div>
+							<div class="our-team__box-social">
+								<a class="our-team__social-link" href="#">
+									<div class="our-team__social-wrap">
+										<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M14.6633 14.6667V14.6661H14.6667V9.77614C14.6667 7.38392 14.1517 5.54114 11.355 5.54114C10.0106 5.54114 9.10833 6.27892 8.74 6.97836H8.70111V5.76447H6.04944V14.6661H8.81055V10.2584C8.81055 9.09781 9.03055 7.97558 10.4678 7.97558C11.8839 7.97558 11.905 9.30003 11.905 10.3328V14.6667H14.6633Z" fill="#111111"/>
+											<path d="M1.55334 5.76501H4.31779V14.6667H1.55334V5.76501Z" fill="#111111"/>
+											<path d="M2.93449 1.33337C2.0506 1.33337 1.33337 2.0506 1.33337 2.93449C1.33337 3.81837 2.0506 4.5506 2.93449 4.5506C3.81837 4.5506 4.5356 3.81837 4.5356 2.93449C4.53504 2.0506 3.81782 1.33337 2.93449 1.33337V1.33337Z" fill="#111111"/>
+										</svg>
+									</div>
+								</a>
+							</div>
+						</div>
+					</div>
+
 				</div>
 			</div>
 		</div>
